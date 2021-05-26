54e9e410c32a9525bb4d47b662a06f1342b87761
Quentin Pénault <quentinpenault@live.fr>
2021-05-26 16:48:38 +0200

    Improve algorithm and fix some issues
    Update eslintrc preferences

--- index.ts
+++ index.ts
@@ -11,117 +11,149 @@ class Block {
 	id: string;
 	html: string;
 	multiLine: boolean = false;
+	canContain: boolean = true;
 }
 
 class TITLE_1 extends Block {
-  constructor() {
-    super();
-    this.id = "#";
-    this.html = "h1";
-  }
+	constructor() {
+		super();
+		this.id = "#";
+		this.html = "h1";
+	}
 }
 
 class TITLE_2 extends Block {
-  constructor() {
-    super();
-    this.id = "##";
-    this.html = "h2";
-  }
+	constructor() {
+		super();
+		this.id = "##";
+		this.html = "h2";
+	}
 }
 
 class TITLE_3 extends Block {
-  constructor() {
-    super();
-    this.id = "###";
-    this.html = "h3";
-  }
+	constructor() {
+		super();
+		this.id = "###";
+		this.html = "h3";
+	}
 }
 
 class PARAGRAPH extends Block {
-  constructor() {
-    super();
-    this.id = "";
-    this.html = "p";
-  }
+	constructor() {
+		super();
+		this.id = "";
+		this.html = "p";
+		this.multiLine = true;
+		this.canContain = false;
+	}
 }
 
 class CODE extends Block {
-  constructor() {
-    super();
-    this.id = "```$";
-    this.html = "code";
-  }
+	constructor() {
+		super();
+		this.id = "```$";
+		this.html = "code";
+		this.canContain = false;
+	}
 }
 
 const blocks: Array<Block> = [new TITLE_1, new TITLE_2, new TITLE_3, new CODE];
 
+var json;
+var objects = [];
+
 function identify(line: string, index: number = 0, identifier: string = ""): Block | null {
-  if (index > line.length) {
-    // Block is a paragraph or a bad-formatted line
-    return new PARAGRAPH;
-  }
+	if (index > line.length) {
+		// Block is a paragraph or a bad-formatted line
+		return new PARAGRAPH;
+	}
 
-  let substr = line.substring(index, index + 1);
+	let substr = line.substring(index, index + 1);
 
-  // Stop identification when space char detected
-  if (substr === " ") {
-    for (let block of blocks) {
-      if (identifier !== block.id) {
+	// Stop identification when space char detected
+	if (substr === " ") {
+		for (let block of blocks) {
+			if (identifier !== block.id) {
+				// TODO : return block paragraph instead
 
-        // TODO : return block paragraph instead
+				continue;
+			} else {
+				return block;
+			}
+		}
+	}
 
-        continue;
-      } else {
-        return block;
-      }
-    }
-  }
+	identifier += substr;
 
-  identifier += substr;
-
-  return identify(line, index + 1, identifier);
+	return identify(line, index + 1, identifier);
 }
 
 function read(line: string, block: Block): string {
-  if (block.id.length) {
-    return line.substring(block.id.length + 1);
-  }
+	if (block.id.length) {
+		return line.substring(block.id.length + 1);
+	}
 
-  return line;
+	return line;
 }
 
-function saveToJson(text: string, block: Block) {
-  if (json[block.html]) {
-    return;
-  }
+function convertToObjects(text: string, block: Block) {
+	if (block.canContain) {
+		objects.push({ [block.html]: { label: text, content: {} } });
+	} else {
+		objects.push({ [block.html]: text });
+	}
+}
 
-  json[block.html] = text;
+function saveToJson(child: Object, objects: Array<Object>, index: number) {
+	if (index < 0) return child;
+
+	var parent = objects[index];
+	if (parent[Object.keys(parent)[0]].content) {
+		parent[Object.keys(parent)[0]].content = { ...parent[Object.keys(parent)[0]].content, ...child };
+	} else {
+		parent = child;
+		var childs = [];
+
+		// Fix for Typescript ^3.9 issue (https://github.com/microsoft/vscode/issues/116219)
+		var tmp = JSON.stringify(child);
+		if (tmp.indexOf('content') < 0) {
+			while (index >= 0) {
+				child = objects[index];
+
+				if (child[Object.keys(child)[0]].content) {
+					parent = child;
+					break;
+				}
+				childs.push(child);
+				index--;
+			}
+
+			childs.forEach(child => {
+				parent[Object.keys(parent)[0]].content = { ...parent[Object.keys(parent)[0]].content, ...child };
+			});
+		}
+	}
+
+	return saveToJson(parent, objects, index - 1);
 }
 
 // -------------------------------------------------------
 
-var json = {};
-
 for (let line of md) {
-  if (line.length === 0) continue;
+	if (line.length === 0) continue;
 
-  console.log(line);
+	let block: Block = identify(line);
+	let text = read(line, block);
 
-  let block: Block = identify(line);
-
-  console.log(block);
-
-  let text = read(line, block);
-
-  console.log('------');
-
-  try {
-    saveToJson(text, block);
-  } catch (error) {
-    // return;
-  }
+	try {
+		convertToObjects(text, block);
+	} catch (error) {
+		console.warn(error);
+	}
 }
 
-console.log(json);
+json = saveToJson({}, objects, objects.length - 1);
+
+console.log(JSON.stringify(json));
 
 // https://asciiparait.fr/courses/development-advanced/02-state-machines/
--- text.md
+++ text.md
@@ -9,9 +9,9 @@ Toujours le même paragraphe
 
 Et un nouveau paragraphe !
 
+### Un titre de niv 3
+
 ```sh
 # Un bloc de code en shell
 echo "coucou les lapinous"
 ```
-
-### Un titre de niv 3
